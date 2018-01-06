7fb087502133e600ac9212319cb59d87db9c0b59
Christopher Diep <diep.christopher@gmail.com>
2018-01-06 15:35:53 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,8 @@ require "pry"
 def load_library(yml)
   #meaning => [eng_emo,jap_emo]
   emoticon = YAML.load_file(yml)
+
+  lib_hash = {"get_meaning"=>{},"get_emoticon"=>{}}
 end
 
 def get_japanese_emoticon
